20841dcd223ccc6ac9bfdb3c82f3081595a7806e
lester <lesterc013@gmail.com>
2024-07-29 15:37:07 +0800

    GET request to server using React Query and axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
+import { useQuery } from '@tanstack/react-query'
+import { getNotes } from './requests'
+
 const App = () => {
   const addNote = async (event) => {
     event.preventDefault()
@@ -10,21 +13,31 @@ const App = () => {
     console.log('toggle importance of', note.id)
   }
 
-  const notes = []
+  const result = useQuery({
+    queryKey: ['notes'],
+    queryFn: () => getNotes,
+  })
+  console.log(JSON.parse(JSON.stringify(result)))
 
-  return(
+  if (result.isLoading) {
+    return <div>loading data...</div>
+  }
+
+  const notes = result.data
+
+  return (
     <div>
       <h2>Notes app</h2>
       <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
+        <input name='note' />
+        <button type='submit'>add</button>
       </form>
-      {notes.map(note =>
+      {notes.map((note) => (
         <li key={note.id} onClick={() => toggleImportance(note)}>
-          {note.content} 
+          {note.content}
           <strong> {note.important ? 'important' : ''}</strong>
         </li>
-      )}
+      ))}
     </div>
   )
 }
