8920d1bb3481bf690c0076385c4bc4b851f80780
lester <lesterc013@gmail.com>
2024-07-29 17:14:16 +0800

    POST and PUT requests using mutations

--- src/requests.js
+++ src/requests.js
@@ -1,4 +1,16 @@
 import axios from 'axios'
 
-export const getNotes = () =>
-  axios.get('http://localhost:3001/notes').then((res) => res.data)
+const baseUrl = 'http://localhost:3001/notes'
+
+export const getNotes = async () => {
+  const response = await axios.get(baseUrl)
+  return response.data
+}
+
+export const createNote = (newNote) =>
+  axios.post(baseUrl, newNote).then((res) => res.data)
+
+export const updateNote = async (updatedNote) => {
+  const response = await axios.put(`${baseUrl}/${updatedNote.id}`, updatedNote)
+  return response.data
+}
